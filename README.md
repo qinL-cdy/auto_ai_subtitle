01567ce23416c77928df3cea24fca8bbe9c4e292
qinL <936281510@qq.com>
2023-04-10 02:03:59 +0800

    add result, fix bug

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # auto_ai_subtitle
 
-基于openai/whisper、translate、ffmpeg，自动为视频生成翻译过后的srt字幕文件
+基于openai/whisper、translate、ffmpeg，自动为视频生成翻译过后的srt字幕文件，支持自定义各种语言
 
 ## 使用方法
+安装 `ffmpeg`
+
 安装依赖 `pip install -r requirements.txt`
 
-将配置信息填入 *config.ini*
+将配置信息填入 `config.ini`
 
-然后执行 `python main.py`
+执行 `python main.py`
+
+## 效果
+原视频：
+
+![img.png](img.png)
+
+执行脚本后生成以下文件：
+
+![img_2.png](img_2.png)
+
+为视频选择要添加的字幕文件：
+
+![img_1.png](img_1.png)
--- script/translate_tool.py
+++ script/translate_tool.py
@@ -4,9 +4,24 @@ import re
 
 def __translate(translator, text, n):
     print("\rtranslate line in: ", n + 1, end="")
-    if not re.match(r"[^->\d\n]", text):
+
+    if text == "" or text == '\n':
         return text
-    return translator.translate(text)
+
+    text = text.rstrip('\n')
+    if re.match(r"^[0-9]+$", text):
+        return add_newline_if_missing(text)
+
+    if re.match(r"\d{2}:\d{2}:\d{2},\d{3}\s-->\s\d{2}:\d{2}:\d{2},\d{3}", text):
+        return add_newline_if_missing(text)
+
+    return add_newline_if_missing(translator.translate(text))
+
+
+def add_newline_if_missing(s):
+    if not s.endswith('\n'):
+        s += '\n'
+    return s
 
 
 def translate_file(translator_fun, file1, file2, translator=None):
@@ -14,6 +29,7 @@ def translate_file(translator_fun, file1, file2, translator=None):
         lines = f1.readlines()
         print("translate file total lines: ", len(lines))
         f2.writelines([translator_fun(translator, line, n) for n, line in enumerate(lines)])
+        print("\ntranslate write file done")
 
 
 def do_translate(file1, file2, form, to):
